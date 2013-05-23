8b0f7e01b50f6c2380ca99845cf8d0575356ccda
Phaedra Deepsky <phae.deepsky@gmail.com>
2013-05-23 13:44:35 -0400

    Initial Commit

---
+++ Timer/VWTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWTAppDelegate.h
+//  Timer
+//
+//  Created by Phaedra Deepsky on 2013-05-23.
+//  Copyright (c) 2013 Phaedra Deepsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VWTAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Timer/VWTAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  VWTAppDelegate.m
+//  Timer
+//
+//  Created by Phaedra Deepsky on 2013-05-23.
+//  Copyright (c) 2013 Phaedra Deepsky. All rights reserved.
+//
+
+#import "VWTAppDelegate.h"
+
+@implementation VWTAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
