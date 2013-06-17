73591f1fb95217c3bc1c044edb768e0549862eb9
Phaedra Deepsky <phae.deepsky@gmail.com>
2013-06-17 03:52:05 -0400

    Add plist controller and test case

---
+++ Timer/VWTdevSettingsController.h
@@ -0,0 +1,15 @@
+//
+//  VWTPlistCOntroller.h
+//  Timer
+//
+//  Created by Phaedra Deepsky on 2013-06-17.
+//  Copyright (c) 2013 Phaedra Deepsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VWTdevSettingsController : NSObject
+
+@property NSDictionary *devSettings;
+
+@end
